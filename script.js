d5152ede50a45a72059bfe528323f2618d7e3d18
drakeerv <60192521+drakeerv@users.noreply.github.com>
2022-02-08 17:12:35 +0000

    stuff

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let links = document.getElementsByClassName("clickable-link");
 for (var i = 0; i < links.length; i++) {
     let link = links[i];
     link.addEventListener("click", () => {
-        window.open(link.href,"_blank");
-        alert(link.href);
+        window.open(link.getAttribute("href"),"_blank");
+        alert(link.getAttribute("href"));
     });
 }
