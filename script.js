49337b79bfa75ad11e41d18433e370c49c4d2f6d
drakeerv <60192521+drakeerv@users.noreply.github.com>
2022-02-08 17:15:09 +0000

    stuff

--- script.js
+++ script.js
@@ -3,6 +3,5 @@ for (var i = 0; i < links.length; i++) {
     let link = links[i];
     link.addEventListener("click", () => {
         window.open(link.getAttribute("href"),"_blank");
-        alert(link.getAttribute("href"));
     });
 }
