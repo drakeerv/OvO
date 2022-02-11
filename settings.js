b26fb927cf074349832b1bd7ef02c72065c6c571
drakeerv <60192521+drakeerv@users.noreply.github.com>
2022-02-11 16:30:00 +0000

    stuff

--- settings.js
+++ settings.js
@@ -5,6 +5,15 @@ if (window.localStorage) {
     
     if (!!allowAnalytics) {
         let alreadyAllowedAnalytics = storage.getItem("allowAnalytics");
-        alert(alreadyAllowedAnalytics);
+        
+        if (alreadyAllowedAnalytics === null) {
+            storage.setItem("allowAnalytics", true);
+        } else {
+            allowAnalytics.checked = !!alreadyAllowedAnalytics;
+        }
+
+        allowAnalytics.addEventListener("change", (event) => {
+            storage.setItem("allowAnalytics", event.currentTarget.checked)
+        });
     }
 }
