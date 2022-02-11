aed06e0ef482b0aabc1b7cd2839b122f051d501d
drakeerv <60192521+drakeerv@users.noreply.github.com>
2022-02-11 19:40:51 +0000

    stuff

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ if (window.localStorage) {
             storage.setItem("allowAnalytics", true);
             allowAnalytics.checked = true;
         } else {
-            allowAnalytics.checked = (alreadyAllowedAnalytics === "true");
+            allowAnalytics.checked = (alreadyAllowedAnalytics == "true");
         }
 
         allowAnalytics.addEventListener("change", (event) => {
