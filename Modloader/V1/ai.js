c79f644a143466c0484601e5f019f28134151ecb
drakeerv <60192521+drakeerv@users.noreply.github.com>
2022-03-02 00:19:01 +0000

    stuff

--- Modloader/V1/ai.js
+++ Modloader/V1/ai.js
@@ -6,6 +6,8 @@
     globalThis.sdk_runtime = old;
 
     // Util stuff
+    let clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+    
     let notify = (title, text, image = "./speedrunner.png") => {
         cr.plugins_.sirg_notifications.prototype.acts.AddSimpleNotification.call(
             runtime.types_by_index.find(
@@ -27,47 +29,101 @@
         js.src = src;
     };
 
-    let getPlayer = () =>
-        playerType.instances.filter(
-            (x) => x.instance_vars[17] === "" && x.behavior_insts[0].enabled
-        )[0];
+    let getPlayer = () => {
+                return runtime.types_by_index
+                    .filter(
+                        (x) =>
+                        !!x.animations &&
+                        x.animations[0].frames[0].texture_file.includes("collider")
+                    )[0]
+                    .instances.filter(
+                        (x) => x.instance_vars[17] === "" && x.behavior_insts[0].enabled
+                    )[0];
+            };
+
+    let getFlag = () => {
+                return runtime.types_by_index.find(
+                    (x) =>
+                    x.name === "EndFlag" ||
+                    (x.plugin instanceof cr.plugins_.Sprite &&
+                        x.all_frames &&
+                        x.all_frames[0].texture_file.includes("endflag"))
+                ).instances[0];
+            };
 
     let ai = {
-        init () {
-            runtime.tickMe(this);
-
+        init() {
             this.network_config = {hiddenLayers: [3, 4, 5]};
             this.train_config = {log: false};
             this.enabled = false;
             this.threshold = 0.5;
+            this.prevInputs = {up: Math.random(), down: Math.random(), left: Math.random(), right: Math.random()};
+            this.prevDistance = Infinity;
+            this.failRandomness = 0.2;
 
-            this.network = new brain.NeuralNetwork(this.network_config);
-            this.network.train({input: {}, output: {up: 0, down: 1, left: 0, right: 0, restart: 0}}, this.train_config); // figure out what data to send, possible position, solid, flag, spike
-
+            runtime.tickMe(this);
+            
             notify("Mod loaded", "AI mod loaded");
             globalThis.ovoAi = this;
         },
-        playInputs (inputs) {
-            inputs.forEach((input) => {
-                if (input.restart > this.threshold) c2_callFunction("Menu > Replay");
-                else if (input.up > this.threshold) c2_callFunction("Controls > Buffer", ["Jump"]);
-                else if (input.down > this.threshold) c2_callFunction("Controls > Down");
-                else if (input.left > this.threshold) c2_callFunction("Controls > Left");
-                else if (input.right > this.threshold) c2_callFunction("Controls > Right");
-            });  
+        playInputs(inputs) {
+            //if (inputs.restart > this.threshold) c2_callFunction("Menu > Replay");
+            if (inputs.up > this.threshold) c2_callFunction("Controls > Buffer", ["Jump"]);
+            if (inputs.down > this.threshold) c2_callFunction("Controls > Down");
+            if (inputs.left > this.threshold) c2_callFunction("Controls > Left In");
+            else if (inputs.left <= this.threshold) c2_callFunction("Controls > Left Out");
+            if (inputs.right > this.threshold) c2_callFunction("Controls > Right In");
+            else if (inputs.right <= this.threshold) c2_callFunction("Controls > Right Out");
         },
-        tick () {
-            if (this.enabled) {
-                this.playInputs(this.network.run({}));
-            }
-        },
-        start () {
+        
+        start() {
+            this.network = new brain.NeuralNetwork(this.network_config);
+            this.network.train([{input: {}, output: {up: Math.random(), down: Math.random(), left: Math.random(), right: Math.random()}}], this.train_config); // figure out what data to send, possible position, solid, flag, spike
             this.enabled = true;
         },
-        stop () {
+        
+        stop() {
+            this.prevInputs = {up: Math.random(), down: Math.random(), left: Math.random(), right: Math.random()};
+            this.prevDistance = Infinity;
+            delete this.network;
             this.enabled = false;
+        },
+
+        addRandom(json) {
+            newJson = {}
+
+            Object.entries(json).forEach((item) => {
+                newJson[item[0]] = clamp(item[1] + ((Math.random() - 0.5) * this.failRandomness * 2), 0, 1);
+            });
+
+            return newJson;
+        },
+
+        tick() {
+            let player = getPlayer();
+            let flag = getFlag();
+            let layout = runtime.running_layout;
+            
+            if (this.enabled && player && flag) {
+                let beginDistance = Math.sqrt((flag.x - player.x) ** 2, (flag.y - player.y) ** 2);
+
+                let pos = {playerx: player.x / layout.width, playery: player.y / layout.height, flagx: flag.x / layout.width, flagy: flag.y / layout.height}
+                let inputs = this.network.run({...this.prevInputs, ...pos});
+                this.playInputs(inputs);
+                this.prevInputs = inputs;
+                
+                let endDistance = Math.sqrt((flag.x - player.x) ** 2, (flag.y - player.y) ** 2);
+                
+                if (endDistance > beginDistance && endDistance < this.prevDistance) {
+                    this.network.train([{input: {...this.prevInputs, ...pos}, output: {...inputs}}]);
+                } else {
+                    this.network.train([{input: {...this.prevInputs, ...pos}, output: {...this.addRandom(inputs)}}]);
+                }
+
+                this.prevDistance = endDistance;
+            }
         }
     };
 
-    addScript("https://unpkg.com/brain.js@latest/dist/brain-browser.min.js", "brainJs", ai.init.bind(ai))
-})():
+    addScript("https://unpkg.com/brain.js@latest/dist/brain-browser.min.js", "brainJs", ai.init.bind(ai));
+})();
