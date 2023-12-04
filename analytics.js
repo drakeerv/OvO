a90c7a9fbec1a160345d6a9b50aa0269d5eb5795
drakeerv <drakeerv@outlook.com>
2023-12-04 08:24:57 -0500

    Add better analytics and one more site

--- analytics.js
+++ analytics.js
@@ -5,16 +5,6 @@ if (window.localStorage) {
             dataLayer.push(arguments)
         }
         window.dataLayer = window.dataLayer || [], gtag("js", new Date);
-        const a = {
-                "ovo.drakefletcher2.repl.co": "G-FYYGF5JH2F",
-                "ovo-modded.netlify.app": "G-VE92Z9GXT8",
-                "ovo.drakeerv.com": "G-ZSPL1SPBYM",
-                "ovo-5yu.pages.dev": "G-CKWDTVZ685",
-                "ovo-modloader.github.io": "G-KNTWWPQSG2",
-                "ovo.onrender.com": "G-9HX201XE1D",
-                "ovo-alpha.vercel.app": "G-X45WR1XH0B"
-            },
-            o = window.location.hostname.toLowerCase();
-        a[o] && gtag("config", a[o])
+        gtag("config", "G-ZSPL1SPBYM")
     }
 }
